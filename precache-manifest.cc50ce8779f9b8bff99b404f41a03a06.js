203090211116149154f6a2f0cccb21a8368182e0
Vignesh Iyer <vigneshiyer94@gmail.com>
2020-10-20 13:58:02 +0530

    Updates

---
+++ precache-manifest.cc50ce8779f9b8bff99b404f41a03a06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddcf70d6a148d454b13351bf43695485",
+    "url": "/joke-guesser-game/index.html"
+  },
+  {
+    "revision": "ca8e51a2ff2127a4b5b9",
+    "url": "/joke-guesser-game/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6514370c046657088a3f",
+    "url": "/joke-guesser-game/static/js/2.4c49d1e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/joke-guesser-game/static/js/2.4c49d1e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca8e51a2ff2127a4b5b9",
+    "url": "/joke-guesser-game/static/js/main.d962cbd1.chunk.js"
+  },
+  {
+    "revision": "864dbb78ff5e2ac7ddb5",
+    "url": "/joke-guesser-game/static/js/runtime-main.f21e1847.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/joke-guesser-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
